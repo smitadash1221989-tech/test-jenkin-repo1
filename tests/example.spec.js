df7460de0682300d8fce63d173c08c96dfe38c4a
smita dash <smita.dash1221989@gmail.com>
2026-08-07 15:25:53 +0530

    test1

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,7 +16,6 @@ test('get started link', async ({ page }) => {
  
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-   console.log("the test is passed");
-   console.log("the test is passed");
+  
 
 });
